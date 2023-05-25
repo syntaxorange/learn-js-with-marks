bcdbbd90642ac1448450b3733c160ea1a6ff325b
syntaxorange <syntaxorange@gmail.com>
2023-05-25 10:10:53 +0300

    init

---
+++ craco.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      // TODO: remove
+      webpackConfig.optimization.minimize = false;
+      const htmlWebpackPluginInstance = webpackConfig.plugins.find(
+        (webpackPlugin) => webpackPlugin instanceof HtmlWebpackPlugin
+      );
+
+      if (htmlWebpackPluginInstance) {
+        htmlWebpackPluginInstance.userOptions.excludeChunks = ["content"];
+      }
+      
+      return {
+        ...webpackConfig,
+        entry: {
+          main: [env === 'development' &&
+            require.resolve('react-dev-utils/webpackHotDevClient'), paths.appIndexJs].filter(Boolean),
+          content: './src/content/content.js'
+        },
+        output: {
+          ...webpackConfig.output,
+          filename: 'static/js/[name].js',
+        },
+        optimization: {
+          ...webpackConfig.optimization,
+          runtimeChunk: false,
+        }
+      }
+    },
+  }
+}
